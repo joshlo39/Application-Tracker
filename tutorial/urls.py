219e99ed10227df99dc2e64a796e80356f6bd39a
SummerHub96 <128830383+SummerHub96@users.noreply.github.com>
2023-11-28 20:51:57 -0500

    Apply to Internship, view internship applicants.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -31,17 +31,20 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('jobs/', views.JobListView.as_view()), #all jobs
     path('jobs/<job_id>/', views.JobDetailView.as_view()),
-    path('jobs/<job_id>/applicants/', views.view_applicants),    
+    path('jobs/<job_id>/applicants/', views.view_applicants),  
+    path('jobs/<job_id>/applicants/<applicant_id>/', views.update_job_application_status),      
     path('jobs/<job_id>/apply/', views.ApplyView),
     path('my_jobs/', views.my_jobs.as_view()),
-    path('my_jobs/<job_id>/', views.my_jobs.as_view()),
+    # path('my_jobs/<job_id>/', views.my_jobs.as_view()),
     path('jobs/state/<str:input_state>/',views.JobListView.as_view()), #filter by state
     path('jobs/city/<str:input_city>/',views.JobListView.as_view()), #filter by city
     path('jobs/state/<str:input_state>/city/<str:input_city>',views.JobListView.as_view()),
     path('jobs/open/', views.view_open),    
     path('internships/', views.InternshipListView.as_view()),   
-    path('internships/<internship_id>/', views.InternshipDetailView.as_view()),   
+    path('internships/<internship_id>/', views.InternshipDetailView.as_view()), 
+    path('internships/<internship_id>/apply/', views.InternApplyView),     
     path('internships/<internship_id>/applicants/', views.view_internship_applicants),
+    path('internships/<internship_id>/applicants/<applicant_id>', views.view_internship_applicants),
     path('my_internships/', views.my_internships.as_view()),
     path('my_internships/<internship_id>/', views.my_internships.as_view()),    
     path('accounts/', include('django.contrib.auth.urls')),
